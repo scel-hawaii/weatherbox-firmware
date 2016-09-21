d6591a3ed3f52d67a51360c08f5225f680b97380
Nathan Lam <nathancy@hawaii.edu>
2016-09-20 16:24:05 -1000

    Updated apogee header file

--- src/gen_dragonfruit/gd_dev_apogee_sp212.cpp
+++ src/gen_dragonfruit/gd_dev_apogee_sp212.cpp
@@ -1,13 +1,13 @@
-#include "gd_dev_apogee_sp212.h"
+#include "gd_dev_apogee_sp215.h"
 
-void gd_dev_apogee_sp212_open(void){
-    pinMode(_PIN_GD_APOGEE_SP212_, INPUT);
+void gd_dev_apogee_sp215_open(void){
+    pinMode(_PIN_GD_APOGEE_SP215_, INPUT);
 }
 
-int gd_dev_apogee_sp212_read(void){
+int gd_dev_apogee_sp215_read(void){
     int value = 555;
     #ifndef SEN_STUB
-    value = analogRead(_PIN_GD_APOGEE_SP212_);
+    value = analogRead(_PIN_GD_APOGEE_SP215_);
     #endif
     return value;
 }
--- src/gen_dragonfruit/gd_dev_apogee_sp212.h
+++ src/gen_dragonfruit/gd_dev_apogee_sp212.h
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-#define _PIN_GD_APOGEE_SP212_ A2
+#define _PIN_GD_APOGEE_SP215_ A2
 
-#ifndef GD_DEV_APOGEE_SP212_H
-#define GD_DEV_APOGEE_SP212_H
-void gd_dev_apogee_sp212_open(void);
-int gd_dev_apogee_sp212_read(void);
+#ifndef GD_DEV_APOGEE_SP215_H
+#define GD_DEV_APOGEE_SP215_H
+void gd_dev_apogee_sp215_open(void);
+int gd_dev_apogee_sp215_read(void);
 #endif
