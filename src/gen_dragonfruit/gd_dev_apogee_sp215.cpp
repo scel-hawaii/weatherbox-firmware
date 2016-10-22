60c99c4cd1eea0c3ec0bff799b7cace47a724754
Nathan Lam <nathancy@hawaii.edu>
2016-10-21 17:58:56 -1000

    Dragonfruit sp215 and solar panel fixes

--- src/gen_dragonfruit/gd_dev_apogee_sp215.cpp
+++ src/gen_dragonfruit/gd_dev_apogee_sp215.cpp
@@ -1,16 +1,21 @@
 #include "gd_dev_apogee_sp215.h"
 
 void gd_dev_apogee_sp215_open(void){
-    Wire.begin(9600);
+    
+    /* Initiate Wire library and join I2C bus as slave */
+    Wire.begin(0x48);
+    
+    /* Enable ready-bit and select 16-bit sample rate */
+    Wire.write(0x88);
+
 }
 
 uint32_t gd_dev_apogee_sp215_read(void){
     uint32_t value = 555;
     #ifndef SEN_STUB
-    Wire.beginTransmission(_PIN_GD_APOGEE_SP215_);
 
-    /* Enable ready-bit and select 16-bit sample rate */
-    Wire.write(0x88);
+    /* Begin transmission to the I2C slave device with the given address */
+    Wire.beginTransmission(0x48);
 
     /* Communicate through I2C */
     Wire.requestFrom(_PIN_GD_APOGEE_SP215_, 3);
@@ -21,9 +26,8 @@ uint32_t gd_dev_apogee_sp215_read(void){
         value += Wire.read();
     }
     Wire.endTransmission();
-    /* Analog to digital conversion with 16 bit-resolution */   
-    /* Divide by 0xFFFF to convert 16-bit ADC reading, 5 V is reference voltage */
-    /* Physical voltage divider implemented to prevent saturation of signal. Apogee SP215 has a maximum output voltage of 5V but the MCP3425 only allows a max input of 2.048V. Voltage divider over .973kohm and 2.167kohm resistor used to decrease amplitude of signal from Apogee. Use a scaling factor of 3.22713257965 to account for this voltage divider and calculate the value of the original signal. */
+    
+    /* Analog to digital conversion with 16 bit-resolution. Multiply by 5V reference voltage then divide by 0xFFFF to convert the 16-bit ADC reading to voltage. The Apogee SP215 sensor has a maximum output voltage of 5V but the MCP3425 ADC converter only allows a max input reading of 2.048V. To prevent a saturated signal reading, A physical voltage divider over R1 (.973 kohm) and R2 (2.167 kohm) is used to decrease the signal amplitude from the Apogee SP215. To account for this voltage divider, a scaling factor of 3.227 is used. */
     value = (3.227 * (value*5000.00)/(0xFFFF)) * 1000; //Convert to microvolts
     #endif
     return value;
--- src/gen_dragonfruit/gd_dev_spanel.cpp
+++ src/gen_dragonfruit/gd_dev_spanel.cpp
@@ -7,7 +7,8 @@ void gd_dev_spanel_open(void){
 int gd_dev_spanel_read(void){
     float value = 555.0;
     #ifndef SEN_STUB
-    /* Physical voltage divider implemented to prevent saturation of signal. Solar panel has a maximum output voltage of 6 V but ATmega328P has maximum input of 5 V. Use a scaling factor of 2 to account for voltage divider. */
+    
+    /* The solar panel has a maximum output voltage of 6V but the ATmega328P microcontroller only allows a maximum input voltage of 5V. To prevent a saturated signal reading, A physical voltage divider is implemented on the board. To account for this voltage divider, a scaling factor of 2 is used. */
     value = 2.0 * (float)analogRead(_PIN_GD_SPANEL_) *(5000.0/1023);
     #endif
     return value;
