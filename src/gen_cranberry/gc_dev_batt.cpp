98033c4b709fc9df5eb700a27b67877e3bf82227
Dylan Tokita <dtokita@hawaii.edu>
2016-10-25 15:24:43 -1000

    Update comment for battery scaling factor

--- src/gen_cranberry/gc_dev_batt.cpp
+++ src/gen_cranberry/gc_dev_batt.cpp
@@ -10,10 +10,12 @@ uint16_t gc_dev_batt_read(void){
     uint16_t value = 4000;
 
     #ifndef SEN_STUB
-    /* Scaling factor of 2.0 is used because of the voltage divider
-    placed in the circuit (Note: Cranberry v3.5.0 Schematic incorrect 
-    R21 and R20 are described to have values of 150k and 51k respectively,
-    these values are the same in hardware. */
+    /* Multiply the result by 2.0 because the ADC is connected to a voltage divider circuit
+    that halves the original battery voltage.
+
+    Note: the cranberry v3.5.0 schematic is incorrect because the values for R21 and
+    R20 are described with values of 150k and 51k respectively. In reality, R21 and
+    R20 are equal to each other (the values are still unkown as of 2016-10-24). */
     value = 2.0*((float)adc.readADC_SingleEnded(2)*188.0)/(1000.0);
     #endif
 
