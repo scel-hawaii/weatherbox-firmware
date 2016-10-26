b32d3dd56ee2db549aa3d732e355acae01b68dc8
Dylan Tokita <dtokita@hawaii.edu>
2016-10-25 14:57:00 -1000

    Update comment for scaling factor

--- src/gen_cranberry/gc_dev_batt.cpp
+++ src/gen_cranberry/gc_dev_batt.cpp
@@ -10,6 +10,8 @@ uint16_t gc_dev_batt_read(void){
     uint16_t value = 4000;
 
     #ifndef SEN_STUB
+    /* Scaling factor of 2.0 is used because of the voltage divider
+    placed in the circuit (Note: Cranberry v3.5.0 Schematic incorrect) */
     value = 2.0*((float)adc.readADC_SingleEnded(2)*188.0)/(1000.0);
     #endif
 
