bb54e175ae8bafb4d5cb9318442f492eed828d61
Kenny Luong <luong97@hawaii.edu>
2016-11-25 12:09:17 -1000

    Add basic weatherbox emulator UI
    Currently only working with the ga_stub build; I plan
    to add more changes in later on.

---
+++ emu/server.py
@@ -0,0 +1,75 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from multiprocessing import Process
+from subprocess import call
+app = Flask(__name__)
+import subprocess
+import os
+
+prev_count = 0;
+current_count = 0;
+max_length = 0;
+emu_state = "STOPPED"
+
+emu_proc = 0;
+
+if( not os.path.exists("emu_output.txt")):
+    print "emu_output.txt does not exist"
+    call(["touch emu_output.txt"], shell=True)
+
+def run_emu():
+    call(["stdbuf -oL python2 run_emu.py ga_stub &> emu_output.txt"], shell=True)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/file_length")
+def file_length():
+    length = subprocess.Popen("wc -l < emu_output.txt", shell=True, stdout=subprocess.PIPE).stdout.read()
+    print length
+    return length
+
+# This route grabs new data that is written to the logfile
+@app.route("/tail")
+def tail():
+    tail_ptr = request.args.get("tail_ptr")
+    output = subprocess.Popen("tail emu_output.txt -n " + str(tail_ptr), shell=True, stdout=subprocess.PIPE).stdout.read()
+    return output
+
+@app.route("/state")
+def state():
+    global emu_state
+
+    return emu_state
+
+@app.route("/control/start")
+def start():
+    global emu_proc
+    global emu_state
+
+    emu_proc = Process(target=run_emu)
+    emu_proc.start()
+
+    emu_state = "RUNNING"
+    return "start"
+
+@app.route("/control/stop")
+def stop():
+    global emu_proc
+    global emu_state
+
+    emu_proc.terminate()
+    emu_state = "STOPPED"
+
+    return "stop"
+
+@app.route("/control/clear")
+def clear_txt():
+    call(["cat /dev/null > test.txt"], shell=True)
+
+    return "clear"
+
+
+app.run()
