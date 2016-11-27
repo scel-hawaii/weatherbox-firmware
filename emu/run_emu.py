afa19e73ffcfa6a054fbb7c483f3a0e447e30aac
Kenny Luong <luong97@hawaii.edu>
2016-11-26 20:17:17 -1000

    Clean main run_emu python script up

--- emu/run_emu.py
+++ emu/run_emu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-from multiprocessing import Process
 from subprocess import call
+from subprocess import Popen
 import time
 import serial
 import struct
@@ -8,19 +8,9 @@ import sys
 import os
 import datetime
 
-# Run the emu background
-def run_base_model():
-    # To run the base model you just need to
-    # provide the path for the .elf executable and an .ihex file to
-    # load the bootloader
-    cmd = "emu_base_model/obj-*/emu_base_model.elf emu_base_model/*.ihex"
-    call([cmd], shell=True)
-
-def upload(build_dir):
-    cmd = "cd " + build_dir + " && avrdude -p m328p -c arduino -P /tmp/simavr-uart0 -U flash:w:firmware.hex"
-    print "Running upload command"
-    print cmd
-    call([cmd], shell=True)
+#
+# Check arguments and files
+#
 
 if len(sys.argv) < 2:
     sys.exit("Error, not build specified")
@@ -34,17 +24,24 @@ if os.path.isdir(build_dir) == False:
 if os.path.isfile(build_dir + "/firmware.hex") == False:
     sys.exit("Error, firmware file does not exist. Maybe try compiling? " + firmware_build)
 
+#
+# Main execution
+#
+
 print "Running emulator with build: " + firmware_build
 
-bm_proc= Process(target=run_base_model)
-bm_proc.start()
+launch_emu_cmd = "emu_base_model/obj-*/emu_base_model.elf emu_base_model/*.ihex"
+emu_proc = Popen([launch_emu_cmd], shell=True)
 
+# Sleep to give some time for the emulator model to come online
 time.sleep(2)
 
-upload_proc = Process(target=upload, args=(build_dir,))
-upload_proc.start()
+print "Running upload command"
 
-upload_proc.join()
+upload_cmd = "cd " + build_dir + " && avrdude -p m328p -c arduino -P /tmp/simavr-uart0 -U flash:w:firmware.hex"
+# Use the 'call' command here to block execution until we want to
+# start reading from the virtual serial port
+upload_proc = call([upload_cmd], shell=True)
 
 ser = serial.Serial('/tmp/simavr-uart0', 9600)
 
