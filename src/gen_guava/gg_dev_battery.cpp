386f1745465eeb47e08500f1ceeee9f6d13eb96e
Dylan Tokita <dtokita@hawaii.edu>
2017-03-24 13:46:42 -1000

    Fix existing file names to match naming scheme

--- src/gen_guava/gg_dev_battery.cpp
+++ src/gen_guava/gg_dev_battery.cpp
@@ -1,16 +1,16 @@
 /*******************************
  *
- * File: gg_dev_batt.cpp
+ * File: gg_dev_battery.cpp
  *
  * This module is a driver for checking the battery reading in mV.
  *
  ******************************/
 
-#include "gg_dev_batt.h"
+#include "gg_dev_battery.h"
 
 /******************************
  *
- * Name:        gg_dev_batt_open
+ * Name:        gg_dev_battery_open
  * Returns:     Nothing
  * Parameter:   Nothing
  * Description: Initialize battery reading pins
@@ -18,38 +18,23 @@
  ******************************/
 
 void gg_dev_batt_open(void){
-    pinMode(_PIN_gg_BATT_, INPUT);
+    pinMode(_PIN_GG_BATTERY_, INPUT);
 }
 
 /******************************
  *
- * Name:        gg_dev_batt_read_raw
- * Returns:     Battery reading in mV
- * Parameter:   Nothing
- * Description: Reads battery voltage without conversion
- *
- ******************************/
-
-int gg_dev_batt_read_raw(void){
-    int value;
-    value = analogRead(_PIN_gg_BATT_);
-    return value;
-}
-
-/******************************
- *
- * Name:        gg_dev_batt_read
+ * Name:        gg_dev_battery_read
  * Returns:     Battery reading in mV
  * Parameter:   Nothing
  * Description: Reads battery voltage with conversion
  *
  ******************************/
 
-int gg_dev_batt_read(void){
+int gg_dev_battery_read(void){
     int val = 555;
 
     #ifndef SEN_STUB
-    float raw = (float)analogRead(_PIN_gg_BATT_) * (5.0/1023.0);
+    float raw = (float)analogRead(_PIN_GG_BATTERY_) * (5.0/1023.0);
     val = raw * 1000;
     #endif
 
